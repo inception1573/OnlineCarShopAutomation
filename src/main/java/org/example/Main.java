cd94acff5d1088a632c193f5f62d04c970717263
shahinsmacbook <n.islam1573@gmail.com>
2023-02-24 22:35:29 -0500

    Verify the number of vehicles in the "38 Vehicles Matching" is the same as the actual number of vehicles displayed and its title "Honda 2022".

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,45 @@
 package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.config.DriverManagerType;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
 import java.util.List;
 public class Main {
     private static WebDriver driver;
     private static int implicitWaitInSecond=5000;
+    private static void setup(String url) throws InterruptedException {
+        WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
+        driver=new ChromeDriver();
+        driver.navigate().to(url);
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+    }
     private static void waitForVisibility(By by){
         WebDriverWait wait = new WebDriverWait(driver,implicitWaitInSecond/1000);
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
     }
+
+    private static void waitForClickable(By by){
+        WebDriverWait wait = new WebDriverWait(driver,implicitWaitInSecond/1000);
+        wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
     public static void main(String[] args) throws InterruptedException {
+
+        automateScenario2();
+
+        driver.close();
+    }
+
+    private static void automateScenario1() throws InterruptedException {
+        setup("https://www.roimotors.com/");
         int numberOfCivic=0;
-
-        WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
-        driver=new ChromeDriver();
-        driver.navigate().to("https://www.roimotors.com/");
-
         List<WebElement> dropdownList=driver.findElements(By.className("right-border"));
 
         for(int i=0;i< dropdownList.size()-1;i++)
@@ -70,6 +88,92 @@ public class Main {
                     System.out.println("Passed!!");
                 }
             }
+        }
+    }
+
+    private static void automateScenario2() throws InterruptedException {
+        setup("https://www.roimotors.com/new-inventory/index.htm");
+
+        waitForVisibility(By.xpath("//div[@id='year']//div[@id='year--heading']"));
+        WebElement elementYear= driver.findElement(By.xpath("//div[@id='year']//div[@id='year--heading']"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", elementYear);
+        elementYear.click();
+
+        waitForVisibility(By.id("year-max"));
+        WebElement inputField = driver.findElement(By.id("year-max"));
+        //clear default value
+        String value=inputField.getAttribute("value");
+        if(value!=null) {
+            for (int i = 0; i < value.length(); i++) {
+                inputField.sendKeys(Keys.BACK_SPACE);
+            }
+        }
+
+        inputField.sendKeys("2022");
+        inputField.sendKeys(Keys.TAB);
+
+        List<WebElement> pageWebElementList=driver.findElements(By.xpath("//ul[@class='pagination']//li"));
+        int totalCarDisplayed=0;
+        List<String> titleList=new ArrayList<>();
+        for(int count=0;count<pageWebElementList.size()-2;count++){
+            if(count>0) {
+                JavascriptExecutor js = (JavascriptExecutor)driver;
+                js.executeScript("arguments[0].scrollIntoView({block: 'center', inline: 'nearest'})", pageWebElementList.get(count));
+                pageWebElementList.get(count).click();
+            }
+
+            Thread.sleep(8000);
+
+            waitForVisibility(By.xpath("//li[contains(@class,'vehicle-card vehicle-card-detailed')]//div[contains(@class,'vehicle-card-body')]"));
+            List<WebElement> carWebElementList=driver.findElements(By.xpath("//li[contains(@class,'vehicle-card vehicle-card-detailed')]//div[contains(@class,'vehicle-card-body')]"));
+            totalCarDisplayed+=carWebElementList.size();
+            for(int j=0;j<carWebElementList.size();j++) {
+                    try {
+                        String title = carWebElementList.get(j).findElement(By.xpath("//div[@class='vehicle-card-details-container']/h2/a/span")).getAttribute("innerHTML");
+                        titleList.add(title);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+            }
+        }
+        List<WebElement> spanList =  driver.findElements(By.cssSelector(".my-1>strong>span"));
+        String searchResultText = spanList.get(0).getAttribute("innerHTML");
+        String[] splitedSearchResult= (searchResultText.split(" ",2));
+        int numberOfCarFound=Integer.parseInt(splitedSearchResult[0]);
+
+        //System.out.println("car displayed "+totalCarDisplayed);
+        System.out.println("1. Number of vehicles in the \"38 Vehicles Matching\" is the same as the actual number of vehicles displayed.");
+        if(totalCarDisplayed==numberOfCarFound)
+        {
+            System.out.println("Status: Passed!");
+        }
+        else{
+            System.out.println("Status: Failed!");
+        }
+
+        System.out.println("2. Verify all 38 vehicles listed on the search page has the title \"2022 Honda\"");
+
+        //System.out.println("Title array size: "+titleList.size());
+        boolean result=true;
+        for (int index=0;index< titleList.size();index++)
+        {
+            //System.out.println("---------------------------------------------------------------");
+            String title=titleList.get(index).trim();
+            //System.out.println(title);
+
+            if(!title.equals("2022 Honda")){
+                System.out.println(titleList.get(index).trim());
+                result=false;
+                break;
+            }
+        }
+
+        if(numberOfCarFound==titleList.size()&&result==true)
+        {
+            System.out.println("Status: Passed!");
+        }
+        else {
+            System.out.println("Status: Failed!");
         }
     }
 }
